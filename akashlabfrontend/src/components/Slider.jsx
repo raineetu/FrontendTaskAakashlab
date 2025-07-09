91bcaeb80d4b49724b835cac9d7b10753b2d70e8
Neetu Rai <raineetu0070@gmail.com>
2025-07-09 18:45:11 +0545

    style:fixed the css

--- akashlabfrontend/src/components/Slider.jsx
+++ akashlabfrontend/src/components/Slider.jsx
@@ -3,13 +3,17 @@ import { assets } from "../constants/constants";
 const Slider = () => {
   return (
     <>
-      <div className="pt-34 flex justify-around items-center">
-        <div>
-          <p>Team of Professionals</p>
-          <p>Our expert team is committed to your success.</p>
+      <div className="flex flex-col md:flex-row justify-around items-center pt-28">
+        <div className="space-y-8 text-center">
+          <p className="text-green-600 text-3xl md:text-5xl font-bold">
+            Team of Professionals
+          </p>
+          <p className="text-gray-600 text-xl md:text-2xl font-semibold">
+            Our expert team is committed to your success.
+          </p>
         </div>
         <div>
-          <img src={assets.hero} className="w-140 h-140" />
+          <img src={assets.hero} className="w-120 h-120 md:w-150 md:h-150" />
         </div>
       </div>
     </>
