5cecd9dbf0caa88ab7ae9b2e9b04246e146d0597
Neetu Rai <raineetu0070@gmail.com>
2025-07-09 16:17:03 +0545

    refactor: code clean up

--- akashlabfrontend/src/App.jsx
+++ akashlabfrontend/src/App.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
