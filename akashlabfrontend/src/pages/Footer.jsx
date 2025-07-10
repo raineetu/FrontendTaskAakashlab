68d8f7c75a363e4cf0d3e7ae43df488f96fb9a37
Neetu Rai <raineetu0070@gmail.com>
2025-07-10 22:33:33 +0545

    feat:added link to social media

--- akashlabfrontend/src/pages/Footer.jsx
+++ akashlabfrontend/src/pages/Footer.jsx
@@ -1,9 +1,33 @@
-import { Facebook, Instagram, Linkedin, Twitter } from "lucide-react";
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 import { assets } from "../constants/constants";
+
 const Footer = () => {
+  const socialMediaLinks = [
+    {
+      name: "Facebook",
+      icon: <FaFacebook />,
+      url: "https://www.facebook.com",
+    },
+    {
+      name: "Instagram",
+      icon: <FaInstagram />,
+      url: "https://www.instagram.com",
+    },
+    {
+      name: "LinkedIn",
+      icon: <FaLinkedin />,
+      url: "https://www.linkedin.com",
+    },
+    {
+      name: "Twitter",
+      icon: <FaTwitter />,
+      url: "https://www.twitter.com",
+    },
+  ];
+
   return (
     <footer className="bg-gray-900 text-white px-6 md:px-16 lg:px-24 py-12">
-      <div className="flex flex-col md:flex-row justify-between gap-10">
+      <div className="flex flex-col md:flex-row md:justify-between gap-10">
         {/* Logo and Description */}
         <div className="flex-1 space-y-4 text-center md:text-left">
           <div className="flex justify-center md:justify-start items-center space-x-2">
@@ -16,10 +40,10 @@ const Footer = () => {
           </p>
         </div>
 
-        {/* Navigation Links */}
-        <div className="flex-1 text-center md:text-left">
+        {/* Navigation  */}
+        <div className="flex-1 text-center ">
           <h3 className="text-lg font-semibold mb-4">Quick Links</h3>
-          <ul className="space-y-2 text-gray-400">
+          <ul className="space-y-2 text-gray-400 flex flex-col justify-center items-center md:flex-row md:space-x-6 md:space-y-0">
             <li className="hover:text-orange-400 cursor-pointer">Home</li>
             <li className="hover:text-orange-400 cursor-pointer">About Us</li>
             <li className="hover:text-orange-400 cursor-pointer">Services</li>
@@ -29,13 +53,20 @@ const Footer = () => {
         </div>
 
         {/* Social Media */}
-        <div className="flex-1 text-center md:text-left">
+        <div className="flex-1 text-center ">
           <h3 className="text-lg font-semibold mb-4">Follow Us</h3>
-          <div className="flex justify-center md:justify-start space-x-4">
-            <Facebook className="hover:text-orange-400 cursor-pointer" />
-            <Twitter className="hover:text-orange-400 cursor-pointer" />
-            <Instagram className="hover:text-orange-400 cursor-pointer" />
-            <Linkedin className="hover:text-orange-400 cursor-pointer" />
+          <div className="flex justify-center  space-x-4">
+            {socialMediaLinks.map((link, index) => (
+              <a
+                key={index}
+                href={link.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-orange-400 cursor-pointer"
+              >
+                {link.icon}
+              </a>
+            ))}
           </div>
         </div>
       </div>
